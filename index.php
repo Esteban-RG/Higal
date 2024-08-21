b07c9960b9bcffd6ac8046ad9acc6d2b12ce80ba
Esteban <ricardoespace@gmail.com>
2024-08-20 22:20:07 -0600

    cambios

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ $error = isset($_GET['error']) ? $_GET['error'] : 'Desconocido';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Start your development with FoodHut landing page.">
-    <meta name="author" content="Devcrud">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
     <title>Higal Restaurante Jardin</title>
 
